1db7e31158da6a09f37bec1c775720df9beaee14
Riku <rikukestila@gmail.com>
2021-12-14 12:41:40 +0200

    Add basic routes

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
+
+
+def index(request):
+    pass
+
+
+def posts(request):
+    pass
+
+
+def post_detail(request):
+    pass
