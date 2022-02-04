6342d2a2314439667235f019ce690442b24a8774
Riku <rikukestila@gmail.com>
2022-02-04 11:57:04 +0200

    Add models to admin

--- app/blog/admin.py
+++ app/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Tag, CustomUser, Author, Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    """Customise the admin panel for a Post"""
+    list_filter = ("author", "tags", "date",)
+    list_display = ("title", "date", "author",)
+    pre_populated_fields = {"slug": ("title",)}
+
+admin.site.register(Tag)
+admin.site.register(CustomUser)
+admin.site.register(Author)
+admin.site.register(Post, PostAdmin)
