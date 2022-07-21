90f0436c1cbc751f35630e5a6faf59db2f131195
Riku <rikukestila@gmail.com>
2022-07-21 12:13:02 +0300

    Add comments

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,4 +1,8 @@
-from django.views.generic import ListView, DetailView
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import ListView
+from django.views import View
 from blog.models import Post
 from .forms import CommentForm
 
@@ -21,15 +25,34 @@ class AllPostsView(ListView):
     ordering = ["-date"]
     context_object_name = "posts"
 
-class SinglePostView(DetailView):
-    template = "blog/post_detail.html"
-    model = Post
+class SinglePostView(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm()
+        }
+        return render(request, "blog/post_detail.html", context)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["posts_tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+        
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("single-post", args=[slug]))
+        
+    
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm()
+        }
+        return render(request, "blog/post_detail.html", context)
+
 
 def get_date(post):
     """Get the date of the post"""
